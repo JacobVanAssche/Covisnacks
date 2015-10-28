72b6f1fafcfb82fd05616c397e0166969130eff1
JacobVanAssche <jake.vanassche@gmail.com>
2015-10-28 13:04:25 -0400

    averaged the measurements

--- ultrasonic.py
+++ ultrasonic.py
@@ -14,18 +14,31 @@ GPIO.output(TRIG, False)
 print "Waiting for sensor to settle"
 time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+num_measurements = 0
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
 
-pulse_duration = pulse_end - pulse_start
-distance = pulse_duration * 17150
-distance = round(distance, 2)
+while True:
+	sum = 0
+	while num_measurements < 10:
+		GPIO.output(TRIG, True)
+		time.sleep(0.00001)
+		GPIO.output(TRIG, False)
 
-print "Distance:",distance,"cm"
+		while GPIO.input(ECHO)==0:
+		  pulse_start = time.time()
+		while GPIO.input(ECHO)==1:
+		  pulse_end = time.time()
+
+		pulse_duration = pulse_end - pulse_start
+		distance = pulse_duration * 17150
+		distance = round(distance, 2)
+		
+		if distance < 20:
+			num_measurements = num_measurements + 1
+			sum = sum + distance
+
+		print "Distance:",distance,"cm"
+		time.sleep(2)
+	average_measurement = sum / num_measurements
+	print average_measurement
 GPIO.cleanup()
