b3ee16adbf2c403841c4412e5ab2ada35997f32a
root <root@raspberrypi.(none)>
2015-10-28 20:12:08 +0000

    added mqtt

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import paho.mqtt.client as mqtt
+
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 23
@@ -14,10 +16,27 @@ GPIO.output(TRIG, False)
 print "Waiting for sensor to settle"
 time.sleep(2)
 
-num_measurements = 0
+def on_connect(client, userdata, rc):
+	print("Connected with result code " + str(rc))
+
+def on_log(pahoClient, obj, level, string):
+	print string
+
+username = "005e04fe-deb5-4520-ae2d-6d54b3aa57ff"
+password = "b4ec9ccb-e5b9-4a68-ba47-b144b7e3eff4"
+
+mqttc = mqtt.Client("390EdA9dEC364bcBb673")
+mqttc.username_pw_set(username, password)
+mqttc.tls_set("/etc/ssl/certs/ca-certificates.crt")
+mqttc.on_connect = on_connect
+mqttc.on_log = on_log
+mqttc.connect("mqtt.covapp.io",8883, 60)
+mqttc.loop_start()
+
 
 
 while True:
+	num_measurements = 0
 	sum = 0
 	while num_measurements < 10:
 		GPIO.output(TRIG, True)
@@ -39,6 +58,8 @@ while True:
 
 		print "Distance:",distance,"cm"
 		time.sleep(2)
+		mqttc.publish("4c7489ed-92c0-4e05-a23d-12cdf514fc6a", distance)
 	average_measurement = sum / num_measurements
-	print average_measurement
+	mqttc.publish("4c7489ed-92c0-4e05-a23d-12cdf514fc6a", average_measurement)
+	print "Average Distance: ",average_measurement
 GPIO.cleanup()
