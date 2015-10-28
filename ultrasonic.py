464052a1faf81b28a975e7c46b6c009db1afe040
root <root@raspberrypi.(none)>
2015-10-28 23:34:36 +0000

    changed stuff

--- ultrasonic.py
+++ ultrasonic.py
@@ -4,6 +4,8 @@ import paho.mqtt.client as mqtt
 import base64
 import ssl
 import json
+import uuid
+
 
 GPIO.setmode(GPIO.BCM)
 
@@ -25,6 +27,12 @@ def on_connect(client, userdata, rc):
 def on_log(pahoClient, obj, level, string):
 	print string
 
+def on_message(client, userdata, msg):
+	print(msg.topic + " " + str(msg.payload))
+
+def on_publish(client, userdata, mid):
+	print("mid: " + str(mid))
+
 username = "005e04fe-deb5-4520-ae2d-6d54b3aa57ff"
 password = "b4ec9ccb-e5b9-4a68-ba47-b144b7e3eff4"
 Qos=0
@@ -34,6 +42,8 @@ mqttc.username_pw_set(username, password)
 mqttc.tls_set("/etc/ssl/certs/ca-certificates.crt",certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1, ciphers=None)
 mqttc.on_connect = on_connect
 mqttc.on_log = on_log
+mqttc.on_message = on_message
+mqttc.on_publish = on_publish
 mqttc.connect("mqtt.covapp.io",8883, 60)
 mqttc.loop_start()
 
@@ -42,7 +52,7 @@ mqttc.loop_start()
 while True:
 	num_measurements = 0
 	sum = 0
-	while num_measurements < 10:
+	while num_measurements < 1:
 		GPIO.output(TRIG, True)
 		time.sleep(0.00001)
 		GPIO.output(TRIG, False)
@@ -67,14 +77,14 @@ while True:
 	average_measurement = sum / num_measurements
 	_eventMessage = {"distance":average_measurement}
 	encodedMsg = base64.b64encode(str(_eventMessage).encode())
-
 	sendEventMessageObj = {
-		        "messageId": "covsiotprodiotval{{$timestamp}}",
+		        "messageId": uuid.uuid4(),
 			"deviceId":"7e731ef0-28e3-49f4-bb1e-5da836a3a54d",
 			"eventTemplateId":"ec4c75f5-7085-4f7a-9bc9-cfb853858889",
 			"message": encodedMsg,
-			"encodingType": "base64",
+			"encodingType": "base64"
 			}
-	mqttc.publish("4c7489ed-92c0-4e05-a23d-12cdf514fc6a", str(sendEventMessageObj), Qos, retain=False)
+	producer_topic = "4c7489ed-92c0-4e05-a23d-12cdf514fc6a"
+	mqttc.publish(producer_topic, str(sendEventMessageObj), Qos, retain=False)
 	print "Average Distance: ",average_measurement
 GPIO.cleanup()
